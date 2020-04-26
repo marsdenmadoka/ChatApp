fa4eedfac20c0569cb8447ead9172ab24bd6f6f4
marsdenmadoka <marsmadoka98@gmail.com>
2020-04-27 00:23:11 +0300

    added group chat

--- app/src/main/java/marsmadoka98/gmail/whatsapp/GroupsFragment.java
+++ app/src/main/java/marsmadoka98/gmail/whatsapp/GroupsFragment.java
@@ -2,17 +2,36 @@ package marsmadoka98.gmail.whatsapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GroupsFragment extends Fragment {
+    private View groupFragmentView;
+    private ListView listView;
+    private ArrayAdapter<String>arrayAdapter;
+    private ArrayList<String>list_of_groups = new ArrayList<>();
+    private DatabaseReference GroupRef;
 
     public GroupsFragment() {
         // Required empty public constructor
@@ -22,8 +41,41 @@ public class GroupsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_groups, container, false);
+        // Inflate the layout for this fragment working with ListView in Fragments
+        groupFragmentView=inflater.inflate(R.layout.fragment_groups, container, false);
+       GroupRef= FirebaseDatabase.getInstance().getReference().child("Groups"); //note the name must be the same with that in the database else it will crash and never work
+
+        listView=(ListView)groupFragmentView.findViewById(R.id.list_view);
+       arrayAdapter=new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,list_of_groups);
+       listView.setAdapter(arrayAdapter);
+
+       RetriveAndDisplayGroups();
+
+        return groupFragmentView;
+    }
+
+    public void RetriveAndDisplayGroups(){
+
+        GroupRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Set<String> set= new HashSet<>();//to prevent dupliction of values when database is update eg when new group is added the dataase is update and database will have old values of it
+                //that is when db is updated the list of groups become old and new list is created and therefore if we dint use hashsets our groups list will be duplicated and repeated in the listview
+                Iterator iterator = dataSnapshot.getChildren().iterator();//we using iterator to loop through the names in the db and display them
+                while (iterator.hasNext()){
+               set.add(((DataSnapshot)iterator.next()).getKey()); //set to remove duplication,iterator to display and datasnashot to fetch from db...read collection and generics topic to understand iterator and Hashset
+//get key get all the names
+                }
+                list_of_groups.clear();
+                list_of_groups.addAll(set);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
  
--- app/src/main/java/marsmadoka98/gmail/whatsapp/MainActivity.java
+++ app/src/main/java/marsmadoka98/gmail/whatsapp/MainActivity.java
@@ -1,17 +1,23 @@
 package marsmadoka98.gmail.whatsapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -107,21 +113,65 @@ private FirebaseUser currentUser;
             mAuth.signOut();
             SendUserToLogin();
         }
+        if(item.getItemId()==R.id.create_group){
+           RequestNewGroup();
+        }
         return super.onOptionsItemSelected(item);
     }
-public void SendUserToSettings(){
+
+   public void SendUserToSettings(){
     Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
     startActivity(intent);
     finish();
-
-
-}
+    }
     public void SendUserToLogin(){
         Intent loginIntent = new Intent(MainActivity.this,LogInActivity.class);
         loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
         finish();
+    }
+    public void RequestNewGroup(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,R.style.AlertDialog);
+        builder.setTitle("Enter Group Name");
+        final EditText groupNameField=new EditText(MainActivity.this);
+        groupNameField.setHint("eg Wanachini Group");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String groupName=groupNameField.getText().toString();
+                if(TextUtils.isEmpty(groupName)){
+                    Toast.makeText(MainActivity.this,"Enter Valid Group Name",Toast.LENGTH_SHORT).show();
+                }else{
+                 CreateNewGroup(groupName);
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+ builder.show();
 
     }
+
+    public void  CreateNewGroup(final String groupName){
+ RootRef.child("Groups").child(groupName).setValue("")   //create a RootChild Called Groups with child of groupname
+.addOnCompleteListener(new OnCompleteListener<Void>() {
+    @Override
+    public void onComplete(@NonNull Task<Void> task) {
+        if(task.isSuccessful()){
+            Toast.makeText(MainActivity.this,groupName+"Group Created Successful",Toast.LENGTH_SHORT).show();
+        }else{
+
+            
+        }
+    }
+});
+    }
+
 }
