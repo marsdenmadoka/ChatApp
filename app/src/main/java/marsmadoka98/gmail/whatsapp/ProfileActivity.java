67f95ca7bc1bf1c708c181d2b28cd11fec663b2a
marsdenmadoka <marsmadoka98@gmail.com>
2020-04-29 23:57:52 +0300

    added message request

--- app/src/main/java/marsmadoka98/gmail/whatsapp/ProfileActivity.java
+++ app/src/main/java/marsmadoka98/gmail/whatsapp/ProfileActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,17 +25,24 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
     private String receiverUserID;
+    private String Current_state;
+    private  String senderUserID;
 
     private CircleImageView userProfileimage;
     private TextView userProfileName,userProfilestatus;
     private Button sendMessageRequestButton;
     private DatabaseReference UserRef;
+    private DatabaseReference ChatRequestRef;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+      mAuth=FirebaseAuth.getInstance();
+      senderUserID=mAuth.getCurrentUser().getUid();
 
+        ChatRequestRef=FirebaseDatabase.getInstance().getReference().child("Chat Requests");
         UserRef= FirebaseDatabase.getInstance().getReference().child("Users");
         receiverUserID=getIntent().getExtras().get("visit_user_id").toString();
         //Toast.makeText(this, "userid"+receiverUserID, Toast.LENGTH_SHORT).show();
@@ -43,14 +53,10 @@ public class ProfileActivity extends AppCompatActivity {
         userProfilestatus=findViewById(R.id.visit_profile_status);
         sendMessageRequestButton=findViewById(R.id.send_message_request_button);
 
+        Current_state="new";
+
         RetriveData();
 
-        sendMessageRequestButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
     private void RetriveData() { //fetching data from database
@@ -68,6 +74,8 @@ public class ProfileActivity extends AppCompatActivity {
                       userProfileName.setText(userName);
                       userProfilestatus.setText(userStatus);
 
+                      ManageChatRequest();
+
                   }else{
                         //if the user has no profilePIc
                       String userName=dataSnapshot.child("name").getValue().toString();
@@ -75,6 +83,8 @@ public class ProfileActivity extends AppCompatActivity {
 
                       userProfileName.setText(userName);
                       userProfilestatus.setText(userStatus);
+
+                      ManageChatRequest();
                   }
 
               }
@@ -84,6 +94,66 @@ public class ProfileActivity extends AppCompatActivity {
 
               }
           });
-
     }
+
+    public void  ManageChatRequest(){
+        ChatRequestRef.child(senderUserID) //we want to maintain the friend request so that once we move out of the activity it wont be lost
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+        //sending the request
+        if(!senderUserID.equals(receiverUserID)){ //if sender id is not equal to receiver userID that means you cannot send message for yourself
+            // hence this validation meaaing the senderID must not be equal to the ReceiverUserID
+            sendMessageRequestButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    sendMessageRequestButton.setEnabled(false);//one you click the button it should remain inactive untill the reciever accepts your request
+                    if(Current_state.equals("new")){ //if the friends are new to each other
+                        SendChatRequest();
+
+
+                    }
+
+                }
+            });
+
+        }else{
+            sendMessageRequestButton.setVisibility(View.INVISIBLE); //means that set the set the sendmessage button inivisible in your own account whe you click
+            // the profile actvity. since you cannot send your self a message click your name in the itemView under findfriends activty and see the working it.
+        } }
+
+        public void SendChatRequest(){ //friend request
+        ChatRequestRef.child(senderUserID).child(receiverUserID)
+                .child("request_type").setValue("sent") //works like sending a friend request or how room/busbooking apps work if there is space they accept your request
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                   if(task.isSuccessful()){
+                       ChatRequestRef.child(receiverUserID).child(senderUserID)
+                               .child("request_type").setValue("received")
+                               .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                   @Override
+                                   public void onComplete(@NonNull Task<Void> task) {
+                                       if(task.isSuccessful()){
+
+                                           sendMessageRequestButton.setEnabled(true);
+                                           Current_state="request_sent";
+                                           sendMessageRequestButton.setText("Cancel chat Request");
+                                       }
+
+                                   }
+                               });
+                   }
+                    }
+                });
+
+
+        }
 }
